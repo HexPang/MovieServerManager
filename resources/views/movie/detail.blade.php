40e0aef73e8b9eb3fb7336e80fce82552bae8dc4
HexPang <hexpang@jabstruse.com>
2016-08-28 05:27:57 +0800

    Detail template

---
+++ resources/views/movie/detail.blade.php
@@ -0,0 +1,43 @@
+@extends('body')
+@section('header')
+<style>
+.movie-table {
+}
+.block {
+  display:block;
+}
+</style>
+@endsection
+@section('body')
+    <div class="ali center">
+        <div class="by">
+            <h4 class="ty">
+                {{ $data['title'] }}
+            </h4>
+
+            <div class="ph">
+              <img src="{{ $data['image'] }}">
+            </div>
+            <div class="ph movie-table">
+              <span class="dh">类型</span>
+              @foreach($data['type'] as $type){{ $type }}&nbsp;@endforeach
+            </div>
+
+            <div class="ph movie-table">
+              <span class="dh">国家</span>
+              @foreach($data['country'] as $country){{ $country }}&nbsp>@endforeach
+            </div>
+
+            <div class="ph movie-table">
+              <span class="dh">演员</span>
+              @foreach($data['actor'] as $actor){{ $actor }}&nbsp;@endforeach
+            </div>
+
+            <div class="ph movie-table">
+              @foreach($data['torrent'] as $torrent)
+                <a href="#" class="block">{{ $torrent['file_name'] }}</a>
+              @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
