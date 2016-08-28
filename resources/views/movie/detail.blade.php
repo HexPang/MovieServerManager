e276ed230b1cfacf8e27c3d0334fab585d861cb3
HexPang <hexpang@jabstruse.com>
2016-08-29 02:28:45 +0800

    Updated

--- resources/views/movie/detail.blade.php
+++ resources/views/movie/detail.blade.php
@@ -25,7 +25,7 @@
 
             <div class="ph movie-table">
               <span class="dh">国家</span>
-              @foreach($data['country'] as $country){{ $country }}&nbsp>@endforeach
+              @foreach($data['country'] as $country){{ $country }}&nbsp;@endforeach
             </div>
 
             <div class="ph movie-table">
@@ -34,10 +34,16 @@
             </div>
 
             <div class="ph movie-table">
-              @foreach($data['torrent'] as $torrent)
-                <a href="#" class="block">{{ $torrent['file_name'] }}</a>
+              @foreach($data['torrent'] as $index=>$torrent)
+                <a href="/movie/{{ $data['id'] }}/{{ $index }}" class="block">{{ $torrent['file_name'] }}</a>
               @endforeach
             </div>
+            @if(isset($data['download']))
+            <div class="ph movie-table">
+              <span class="dh">提示</span>
+              {{ $data['download'] }}
+            </div>
+          @endif
         </div>
     </div>
 @endsection
