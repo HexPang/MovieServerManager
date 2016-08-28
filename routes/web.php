e8c43604f23772aae1a4837b4d6becf458f756dc
HexPang <hexpang@jabstruse.com>
2016-08-29 00:39:56 +0800

    Add router

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@
 Route::get('/', 'ViewController@showView');
 Route::get('/{view}/{action}', 'ViewController@showView');
 Route::get('/{view}/{action}/{param}', 'ViewController@showView');
+Route::get('/{view}/{action}/{param}/{param1}', 'ViewController@showView');
