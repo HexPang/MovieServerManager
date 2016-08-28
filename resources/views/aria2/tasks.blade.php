f1f121cf88357ed60f2446c4706fca91b59fb181
HexPang <hexpang@jabstruse.com>
2016-08-29 02:28:31 +0800

    Add stopepd,waiting number

--- resources/views/aria2/tasks.blade.php
+++ resources/views/aria2/tasks.blade.php
@@ -30,13 +30,15 @@
         <div class="by">
             <h4 class="ty">
                 任务状态
+                <span class="stat">停止任务:{{ $data['stat']['numStoppedTotal'] }}</span>
+                <span class="stat">暂停任务:{{ $data['stat']['numWaiting'] }}</span>
                 <span class="stat">活跃任务:{{ $data['stat']['numActive'] }}</span>
                 <span class="stat">下载:{{ $data['stat']['downloadSpeed'] }}/s</span>
                 <span class="stat">上传:{{ $data['stat']['uploadSpeed'] }}/s</span>
             </h4>
             @foreach($data['downloading'] as $file)
               <div class="ph">
-                <a href="/aria2/tasks/{{ $file['gid'] }}/remove" class="stat-action">删除</a>
+                <a href="/aria2/tasks/{{ $file['gid'] }}/forceRemove" class="stat-action">删除</a>
                 <span class="stat">
                   {{ $file['downloadSpeed'] }}/s
                 </span>
