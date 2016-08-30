7364d79c89d8ba27292f9fec5082fa72a045a904
HexPang <hexpang@jabstruse.com>
2016-08-30 23:38:37 +0800

    Add disk usage template

--- resources/views/system/status.blade.php
+++ resources/views/system/status.blade.php
@@ -14,6 +14,49 @@
 @endsection
 @section('body')
     <div class="ali center">
+      <div class="by">
+          <h4 class="ty">
+              磁盘状态
+          </h4>
+          @if(isset($data['disks']))
+            <div class="ud">
+              <div class="eg">
+                <table class="cl" data-sort="table">
+                  <thead>
+                    <tr>
+                      <th class="header">位置</th>
+                      <th class="header">挂载点</th>
+                      <th class="header">总空间</th>
+                      <th class="header">已使用</th>
+                      <th class="header">剩余空间</th>
+                      <th class="header">使用率</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($data['disks'] as $disk)
+                      <tr>
+                        <td>{{ $disk[0] }}</td>
+                        <td>{{ $disk[5] }}</td>
+                        <td>{{ $disk[1] }}</td>
+                        <td>{{ $disk[2] }}</td>
+                        <td>{{ $disk[3] }}</td>
+                        <td>{{ $disk[4] }}</td>
+                      </tr>
+                    @endforeach
+                </tbody>
+                </table>
+              </div>
+            </div>
+          @else
+            <div class="ph">
+              <span class="dy dh red">
+                无法连接至服务器.请检查配置文件.
+              </span>
+              错误
+            </div>
+          @endif
+      </div>
+
         <div class="by">
             <h4 class="ty">
                 服务状态
