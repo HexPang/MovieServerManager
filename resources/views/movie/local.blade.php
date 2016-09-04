a1fc95905ec7680479d179a192c7e1848a7b7dc2
HexPang <hexpang@jabstruse.com>
2016-09-04 18:42:38 +0800

    Remove size

--- resources/views/movie/local.blade.php
+++ resources/views/movie/local.blade.php
@@ -7,13 +7,13 @@
         <thead>
           <tr>
             <th class="header">文件名</th>
-            <th class="header">大小</th>
+            {{-- <th class="header">大小</th> --}}
             <th class="header">操作</th>
           </tr>
         </thead>
         <tbody>
           @foreach($data['movies'] as $movie)
-            <tr><td>{{ $movie['name'] }}</td><td>{{ $movie['size'] }}</td><td><a href="/movie/play/{{ urlencode($movie['file']) }}">播放</a></td></tr>
+            <tr><td>{{ $movie['name'] }}</td><td><a href="/movie/play/{{ urlencode($movie['file']) }}">播放</a></td></tr>
           @endforeach
       </tbody>
       </table>
